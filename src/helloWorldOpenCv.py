833d5e0ba2c9f737a9ced63f5f8073876575f7ed
rustam <SayfovRK@spb.rdi-kvant.ru>
2025-02-26 10:09:21 +0300

    lab1 checking changes

--- src/helloWorldOpenCv.py
+++ src/helloWorldOpenCv.py
@@ -1,34 +1,38 @@
+import cv2 as cv
 import numpy as np
-import cv2
 import imutils
 
-# Create a black image
-img = np.zeros((1920,1080,3), np.uint8)
+white_list = np.zeros((1080,1920,3), dtype=np.uint8) + 0xff
 
-for i in range(1920):
-    for j in range(1080):
-        img[i][j] = (255,255,255)
+#cv.rectangle(white_list, (70, 60), (250, 150), (0x09, 0xff, 0), -1)
+cv.rectangle(white_list, (157, 289), (40, 560), (0, 0x14, 0), -1)
+cv.circle(white_list, (700, 300), 70, (0xf0, 0xff, 0), -1)
+cv.circle(white_list, (800, 500), 120, (0xBC, 0xA0, 0), -1)
+#cv.rectangle(white_list, (65, 80), (90, 60), (0, 0, 0xFA), -1)
+#cv.rectangle(white_list, (2, 2), (70, 90), (0xf0, 0x0f, 0), -1)
 
-# Draw a diagonal blue line with thickness of 5 px
-#cv2.line(img,(0,0),(511,511),(255,0,0),5)
 
-cv2.rectangle(img,(1920//2,1080//2),(510,128),(0,255,0),3)
-cv2.circle(img,(1920//3,1080//3), 63, (0,0,255), -1)
-cv2.ellipse(img,(256,256),(100,100),0,0,360,255,-1)
+gray = cv.cvtColor(white_list, cv.COLOR_BGR2GRAY)
 
-#pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
-#pts = pts.reshape((-1,1,2))
-#cv2.polylines(img,[pts],True,(0,255,255))
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-thresh = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY_INV)[1]
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+thresh = cv.threshold(gray, 0xff, 0xff, cv.THRESH_BINARY_INV)[1]
+
+cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
 cnts = imutils.grab_contours(cnts)
-output = img.copy()
+output = white_list.copy()
+
 for c in cnts:
-    cv2.drawContours(output, [c], -1, (240, 0, 159), 3)
+    cv.drawContours(output, [c], -1, (240, 0, 159), 3)
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img,f'1242 Sayfov Rustam Karomatulloevich found {len(cnts)}',(10,50), font, 1,(0,0,0),2,cv2.LINE_AA)
+cv.putText(
+    output,
+    f"Sayfov Rustam 1242 and I found {len(cnts)} objects",
+    (80, 25),
+    cv.FONT_HERSHEY_SIMPLEX,
+    0.8, (0, 0, 0), 2
+)
 
-cv2.imshow("Image", img)
-cv2.waitKey(1000000)
+
+cv.imshow("Lab1AI", output)
+cv.waitKey(0)
+cv.destroyAllWindows()
